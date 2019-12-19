66eeb0fba3f1b988d3e668514c57fbb9d321eb80
gauravsingh9891 <gauravhacker8800@gmail.com>
2019-12-19 22:40:39 +0530

    Lecture 1

---
+++ basic programs/string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+#include<string.h>
+using namespace std;
+main()
+{
+	string mystring;
+	mystring="Hii Jarvis !!!!";
+	cout<<"\n"<<mystring;
+	mystring="I AM IRON MAN :) ";
+	cout<<"\n"<<mystring;
+	getch();
+}
