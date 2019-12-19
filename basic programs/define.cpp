fe7d7b5a6d6bfc561e21847eef074dd530ba6060
gauravsingh9891 <gauravhacker8800@gmail.com>
2019-12-19 22:52:35 +0530

    Lecture 1

---
+++ basic programs/define.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+#define PI 3.14
+#define NEWLINE "\n"  //ignou book
+using namespace std;
+main()
+{
+	double r=5.0;
+	double circle;
+	circle=2*PI*r;
+	cout<<"Area of circle is :"<<circle;
+	cout<<NEWLINE;
+	cout<<"---It works --";
+	getch();
+}
