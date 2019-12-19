367611a2a168e30b4b6eea593f99ff6f1e1f2e74
gauravsingh9891 <gauravhacker8800@gmail.com>
2019-12-19 22:22:50 +0530

    Lecture 1

---
+++ basic programs/new thing.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+main()
+{
+	int a=5;
+	int b(2); // new thing
+	int result;
+	a=a+3;
+	result=a-b;
+	cout<<"\n Result is : "<<result;
+	getch();
+}
