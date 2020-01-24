87e1f91c8013fb31c47edf417c67c8523797d71d
gauravsingh9891 <gauravhacker8800@gmail.com>
2020-01-25 01:31:15 +0530

    Lecure 9

---
+++ Constructor/Default constructor/Matrix sum of diagonal elements.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+class matrix
+{
+	int i,m,j,d1_sum,d2_sum,a[5][5];
+	public:
+		matrix()
+		{
+			d1_sum=0;
+			d2_sum=0;
+		}
+		
+		void calculate()
+		{
+			cout<<"\n enter the size of matrix :";
+			cin>>m;
+			cout<<"\n enter the elements of matrix in row wise : ";
+			for(i=0;i<3;i++)
+			{
+				for(j=0;j<m;j++)
+				{
+					cin>>a[i][j];
+				}
+			}
+			cout<<"\n lements of matrix in row : "<<endl;
+			for(i=0;i<3;i++)
+			{
+				for(j=0;j<m;j++)
+				{
+					cout<<a[i][j]<<"\t\t";
+				}
+				cout<<endl;
+			}
+		}
+		
+		void show()
+		{
+		   for(i=0;i<3;i++)
+			{
+				for(j=0;j<m;j++)
+				{
+					if(i==j)
+					d1_sum=d1_sum+a[i][j];
+					if(i+j==(m-1))
+					d2_sum=d2_sum+a[i][j];
+				}
+			}
+			cout<<"\n Sum of 1st diagonals elements : "<<d1_sum;
+			cout<<"\n Sum of 2nd diagonals elements : "<<d2_sum;
+		}
+};
+
+main()
+{
+	matrix obj;
+	obj.calculate();
+	obj.show();
+	getch();
+}
